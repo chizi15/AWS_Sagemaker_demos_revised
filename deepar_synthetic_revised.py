cb3e17f113917a6190d7a6500649123a69e18023
Chi Zhang <41175891+chizi15@users.noreply.github.com>
2021-03-02 09:27:33 +0800

    Add files via upload

--- deepar_synthetic_revised.py
+++ deepar_synthetic_revised.py
@@ -7,31 +7,39 @@ import boto3
 import s3fs
 import sagemaker
 from sagemaker.tuner import IntegerParameter, CategoricalParameter, ContinuousParameter, HyperparameterTuner
-
 # from sagemaker import get_execution_role
-np.random.seed(0)
 
 # set aws parameters
-aws_access_key_id = 'your_access_key_id'
-aws_secret_access_key = 'your_secret_access_key'
+aws_access_key_id = 'AKIA3ZMS56MGH7FMNUEF'
+aws_secret_access_key = 'b1lE/mSx/jsA0jSjuxT2u7N2e5g9manR7/D9JC0u'
 region_name = 'cn-northwest-1'
-role = 'your_role'
-sagemaker_session = sagemaker.Session(boto3.session.Session(profile_name='default'))
-bucket = 'your_bucket'
+role = 'arn:aws-cn:iam::810446353164:role/service-role/AmazonSageMaker-ExecutionRole-20200814T152597'
+sagemaker_session = sagemaker.Session(boto3.session.Session(
+    aws_access_key_id = 'AKIA3ZMS56MGH7FMNUEF', aws_secret_access_key = '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'))
+# sagemaker_session = sagemaker.Session(boto3.session.Session(
+#     aws_access_key_id = 'AKIA3ZMS56MGH7FMNUEF', aws_secret_access_key = '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',
+#     region_name = 'cn-northwest-1'))
+# sagemaker_session = sagemaker.Session(boto3.session.Session(region_name = 'cn-northwest-1', profile_name='default'))
+# sagemaker_session = sagemaker.Session()
+bucket = 'sagemaker-cn-northwest-1-810446353164'
 prefix = 'sagemaker/zc/DEMO-deepar'
 s3_data_path = "{}/{}/data".format(bucket, prefix)
 s3_output_path = "{}/{}/output".format(bucket, prefix)
-image_name = sagemaker.image_uris.retrieve(framework='forecasting-deepar', region=region_name, version='1')
+image_name = sagemaker.image_uris.retrieve(framework='forecasting-deepar', region=region_name, version='latest')
+
 
 # set data and model parameters
-freq = 'D'
-prediction_length = 28
-context_length = 28 * 2
-t0 = '2020-01-01'
-data_length = 365 * 3
-num_ts = 4 * 1
-period = 365
+freq = '1W'
+mini_batch_size = 100
+period = 52
+prediction_length = int(period/4)  # The value for hyperparameter 'prediction_length' should be of type integer
+context_length = prediction_length*1  # The value for hyperparameter 'context_length' should be of type integer
+t0 = '2018-01-01'
+data_length = period*3
+num_ts = mini_batch_size * 1
 
+np.random.seed(0)
+plt.figure(figsize=(20,10))
 # generate training and testing data
 time_series = []
 for k in range(num_ts):
@@ -42,18 +50,33 @@ for k in range(num_ts):
     source = level + seas_amplitude * np.sin(time_ticks * (2 * np.pi) / period)
     noise = sig * np.random.randn(data_length)
     data = source + noise
-    index = pd.date_range(t0, periods=data_length, freq='D')
+    index = pd.date_range(t0, periods=data_length, freq=freq)
     time_series.append(pd.Series(data=data, index=index))
-time_series[num_ts - 1].plot()
-plt.show()
 
+# 使time_series_training和time_series一一对应
 time_series_training = []
 for ts in time_series:
     time_series_training.append(ts[:-prediction_length])
-time_series[num_ts - 1].plot(label='test')
-time_series_training[num_ts - 1].plot(label='train', ls=':')
-plt.legend()
-plt.show()
+
+# # 使time_series_training和time_series无关
+# time_series_training = []
+# for k in range(num_ts):
+#     level = 10 * np.random.rand()
+#     seas_amplitude = (0.1 + 0.3 * np.random.rand()) * level
+#     sig = 0.05 * level  # noise parameter (constant in time)
+#     time_ticks = np.array(range(data_length-prediction_length))
+#     source = level + seas_amplitude * np.sin(time_ticks * (2 * np.pi) / period)
+#     noise = sig * np.random.randn(data_length-prediction_length)
+#     data = source + noise
+#     index = pd.date_range(t0, periods=data_length-prediction_length, freq='D')
+#     time_series_training.append(pd.Series(data=data, index=index))
+
+num_of_samples = 10
+for k in range(num_of_samples):
+    time_series[num_ts - num_of_samples:][k].plot(label='actual')  # 只画出最后4条实际值序列，和最后与预测序列对比的实际值序列一一对应
+    time_series_training[num_ts - num_of_samples:][k].plot(label='train', ls=':')  # 只画出最后4条训练序列，和最后的预测序列一一对应
+    plt.legend()
+    plt.show()
 
 
 # convert pd.series(time series) to dict, then to json line
@@ -71,16 +94,16 @@ def series_to_jsonline(ts, cat=None):
 # upload data to s3
 encoding = "utf-8"
 s3filesystem = s3fs.S3FileSystem(key=aws_access_key_id, secret=aws_secret_access_key)
-with s3filesystem.open(s3_data_path + "/train/train-4.json", 'wb') as fp:
+with s3filesystem.open(s3_data_path + "/train/train-400.json", 'wb') as fp:
     for ts in time_series_training:
         fp.write(series_to_jsonline(ts).encode(encoding))
         fp.write('\n'.encode(encoding))
-with s3filesystem.open(s3_data_path + "/test/test-4.json", 'wb') as fp:
+with s3filesystem.open(s3_data_path + "/test/test-400.json", 'wb') as fp:
     for ts in time_series:
         fp.write(series_to_jsonline(ts).encode(encoding))
         fp.write('\n'.encode(encoding))
 
-training_type = 'normal_training'  # normal_training or hypers_tuning
+training_type = 'hypers_tuning'  # normal_training or hypers_tuning
 
 # normal training, no hypers tuning
 if training_type == 'normal_training':
@@ -89,29 +112,35 @@ if training_type == 'normal_training':
         image_uri=image_name,
         role=role,
         instance_count=1,
-        instance_type='ml.m5.xlarge',
+        instance_type='ml.c5.xlarge',
         max_run=3600,
         use_spot_instances=True,
         max_wait=3600,
         # MaxWaitTimeInSeconds(i.e. max_wait) above 3600 is not supported for the given algorithm,
         # and It must be present and be greater than or equal to MaxRuntimeInSeconds(i.e. max_run).
+        checkpoint_s3_uri='s3://sagemaker-cn-northwest-1-810446353164/DeepAR/HPO/model/checkpoint_s3_uri',
+        checkpoint_local_path='/opt/ml/checkpoints',
         base_job_name='DEMO-deepar-synthetic-zc',  # 不能用下划线_
         output_path="s3://" + s3_output_path,
-        tags=[{'Key': 'app', 'Value': 'sagemaker-deepar-demo-test'},
-              {'Key': 'env', 'Value': 'test'},
-              {'Key': 'name', 'Value': 'synthetic-data'},
-              {'Key': 'depart', 'Value': 'rd7'},
-              {'Key': 'manage', 'Value': 'zhangchi'},
-              {'Key': 'input data', 'Value': 'train-4.json'},
+        tags=[{'Key': 'App', 'Value': 'Sagemaker-DeepAR'},
+              {'Key': 'Depart', 'Value': 'RD7'},
+              {'Key': 'Env', 'Value': 'Test'},
+              {'Key': 'Manager', 'Value': 'Zhang Chi'},
+              {'Key': 'Name', 'Value': 'DeepAR-Training'},
+              {'Key': 'input data', 'Value': 'train-400.json'},
               {'Key': 'instance type / workers / parallel / count',
-               'Value': 'ml.m5.xlarge / 4 / 1 / 1'}])  # tags中不能出现中英文逗号
+               'Value': 'ml.c5.xlarge / 4 / 1 / 1'}])  # tags中不能出现中英文逗号
     hyperparameters = {
+        'num_eval_samples': prediction_length,
+        'test_quantiles': [0.5, 0.6, 0.7, 0.8],
+        # '_tuning_objective_metric': 'test:mean_wQuantileLoss',
         "time_freq": freq,
         "context_length": str(context_length),
-        "prediction_length": str(prediction_length),
+        "prediction_length": str(prediction_length),  # prediction_length in predict and training must be the same
         "num_cells": "40",
         "num_layers": "2",
-        "epochs": "100",
+        'embedding_dimension': 10,
+        "epochs": "50",
         "mini_batch_size": "32",
         "learning_rate": "0.001",
         "dropout_rate": "0.1",
@@ -119,8 +148,8 @@ if training_type == 'normal_training':
         'likelihood': 'gaussian'}
     estimator.set_hyperparameters(**hyperparameters)
     data_channels = {
-        "train": "s3://{}/train/train-4.json".format(s3_data_path),
-        "test": "s3://{}/test/test-4.json".format(s3_data_path)}
+        "train": "s3://{}/train/train-400.json".format(s3_data_path),}
+        # "test": "s3://{}/test/test-400.json".format(s3_data_path)}
     estimator.fit(inputs=data_channels, wait=True)
     # deploy endpoint using training model
     job_name = estimator.latest_training_job.name
@@ -132,7 +161,7 @@ if training_type == 'normal_training':
         role=role,
         wait=True,
         name=job_name)
-    # name必须与job_name一致，否则会报"ValueError: Shape of passed values is (28, 9), indices imply (24, 9)"的错误，括号内数字可能不同。
+    # name必须与job_name一致，否则当不同模型的输入数据维度不一致时会报类似于"ValueError: Shape of passed values is (28, 9), indices imply (24, 9)"的错误，括号内数字可能不同。
 
 # hypers tuning
 elif training_type == 'hypers_tuning':
@@ -140,14 +169,17 @@ elif training_type == 'hypers_tuning':
         sagemaker_session=sagemaker_session,
         image_uri=image_name,
         role=role,
-        instance_count=1,
-        instance_type='ml.m5.xlarge',
+        instance_count=2,
+        instance_type='ml.c5.xlarge',
         max_run=3600,
         use_spot_instances=True,
         max_wait=3600,
         base_job_name='DEMO-deepar-synthetic-zc',
         output_path="s3://" + s3_output_path,
         hyperparameters={
+            'num_eval_samples': prediction_length,
+            'test_quantiles': [0.5, 0.6, 0.7, 0.8],
+            # 'tuning_objective_metric': 'test:mean_wQuantileLoss',
             "time_freq": freq,
             "context_length": str(context_length),
             "prediction_length": str(prediction_length),
@@ -159,16 +191,18 @@ elif training_type == 'hypers_tuning':
             "dropout_rate": "0.05",
             "early_stopping_patience": "10",
             'likelihood': 'gaussian'},
-        tags=[{'Key': 'app', 'Value': 'sagemaker-deepar-demo-test'},
-              {'Key': 'env', 'Value': 'test'},
-              {'Key': 'name', 'Value': 'synthetic-data/hyperparameters-tuning'},
-              {'Key': 'depart', 'Value': 'rd7'},
-              {'Key': 'manage', 'Value': 'zhangchi'},
-              {'Key': 'input data', 'Value': 'train-4.json'},
+        tags=[{'Key': 'App', 'Value': 'Sagemaker-DeepAR'},
+              {'Key': 'Depart', 'Value': 'RD7'},
+              {'Key': 'Env', 'Value': 'Test'},
+              {'Key': 'Manager', 'Value': 'Zhang Chi'},
+              {'Key': 'Name', 'Value': 'DeepAR-Training'},
+              {'Key': 'input data', 'Value': 'train-400.json'},
               {'Key': 'instance type / workers / parallel / count', 'Value': 'ml.m5.xlarge / 4 / 1 / 1'},
               {'Key': 'hypers choice', 'Value': 'likelihood: deterministic-L1 / student-T'}])  # tags中不能出现中英文逗号
 
-    hyperparameter_ranges = {'likelihood': CategoricalParameter(["deterministic-L1", 'student-T'])}
+    # hyperparameter_ranges = {'likelihood': CategoricalParameter(["deterministic-L1", 'student-T'])}
+    hyperparameter_ranges = {'embedding_dimension': IntegerParameter(32, 64), 'num_cells': IntegerParameter(32, 64),
+                             'num_layers': IntegerParameter(2, 3)}
     objective_metric_name = 'test:mean_wQuantileLoss'
     tuner = HyperparameterTuner(
         estimator_hyper,
@@ -176,20 +210,20 @@ elif training_type == 'hypers_tuning':
         hyperparameter_ranges,
         strategy='Bayesian',
         objective_type='Minimize',
-        max_jobs=1,
+        max_jobs=2,
         max_parallel_jobs=1,
-        tags=[{'Key': 'app', 'Value': 'sagemaker-deepar-demo-test'},
-              {'Key': 'env', 'Value': 'test'},
-              {'Key': 'name', 'Value': 'synthetic-data/hyperparameters-tuning'},
-              {'Key': 'depart', 'Value': 'rd7'},
-              {'Key': 'manage', 'Value': 'zhangchi'},
-              {'Key': 'input data', 'Value': 'train-4.json'},
+        tags=[{'Key': 'App', 'Value': 'Sagemaker-DeepAR'},
+              {'Key': 'Depart', 'Value': 'RD7'},
+              {'Key': 'Env', 'Value': 'Test'},
+              {'Key': 'Manager', 'Value': 'Zhang Chi'},
+              {'Key': 'Name', 'Value': 'DeepAR-HypersTuning'},
+              {'Key': 'input data', 'Value': 'train-400.json'},
               {'Key': 'instance type / workers / parallel / count',
                'Value': 'ml.m5.xlarge / 4 / 1 / 1'},
               {'Key': 'hypers choice', 'Value': 'likelihood: deterministic-L1 / student-T'}])
     data_channels = {
-        "train": "s3://{}/train/train-4.json".format(s3_data_path),
-        "test": "s3://{}/test/test-4.json".format(s3_data_path)}
+        "train": "s3://{}/train/train-400.json".format(s3_data_path),
+        "test": "s3://{}/test/test-400.json".format(s3_data_path)}
     tuner.fit(inputs=data_channels, wait=True)
     # tuner.fit(inputs=data_channels, wait=False)
     # boto3.client('sagemaker').describe_hyper_parameter_tuning_job(
@@ -231,10 +265,10 @@ class DeepARPredictor(sagemaker.predictor.Predictor):
         Return value: none.
         """
         self.freq = freq
-        self.prediction_length = prediction_length
+        self.prediction_length = prediction_length  # prediction_length in predict and training must be the same
 
-    def predict(self, ts, cat=None, encoding="utf-8", num_samples=100,
-                quantiles=["0.1", '0.2', '0.3', '0.4', "0.5", '0.6', '0.7', '0.8', "0.9"]):
+    def predict(self, ts, cat=None, encoding="utf-8", num_samples=1000,
+                quantiles=["0.1", '0.2', '0.3', '0.4', "0.5", '0.6', '0.7', '0.8', "0.9", '0.95']):
         """
         Requests the prediction of for the time series listed in `ts`, each with the (optional)
         corresponding category listed in `cat`.
@@ -266,24 +300,59 @@ class DeepARPredictor(sagemaker.predictor.Predictor):
         return list_of_df
 
 
-predictor = DeepARPredictor(
-    endpoint_name=endpoint_name,
-    sagemaker_session=sagemaker_session)
+predictor = DeepARPredictor(endpoint_name=endpoint_name, sagemaker_session=sagemaker_session)
 predictor.set_prediction_parameters(freq, prediction_length)
 # predictor.serializer.content_type = "application/json"
 
 # predict data
-list_of_df = predictor.predict(time_series_training[-4:])
-actual_data = time_series[-4:]
+list_of_df = predictor.predict(time_series_training[-num_of_samples:])
+actual_data = time_series[-num_of_samples:]
 for k in range(len(list_of_df)):
     plt.figure(figsize=(12, 6))
-    actual_data[k][-prediction_length - context_length:].plot(label='target')
+    # actual_data[k][-prediction_length - context_length:].plot(label='target')
+    actual_data[k][-prediction_length:].plot(label='target')
     p10 = list_of_df[k]['0.1']
     p90 = list_of_df[k]['0.9']
     plt.fill_between(p10.index, p10, p90, color='y', alpha=0.5, label='80% confidence interval')
     list_of_df[k]['0.5'].plot(label='prediction median')
+    list_of_df[k]['0.95'].plot(label='prediction P95')
+    time_series_training[-num_of_samples:][k].plot(label='input')
     plt.legend()
     plt.show()
 
 # delete endpoint machine
 sagemaker_session.delete_endpoint(endpoint_name)
+
+
+"""deepar train: [ml.p2.xlarge, ml.m5.4xlarge, ml.m4.16xlarge, ml.p4d.24xlarge, ml.c5n.xlarge, ml.p3.16xlarge, ml.m5.large, 
+ml.p2.16xlarge, ml.c4.2xlarge, ml.c5.2xlarge, ml.c4.4xlarge, ml.c5.4xlarge, ml.c5n.18xlarge, ml.g4dn.xlarge, 
+ml.g4dn.12xlarge, ml.c4.8xlarge, ml.g4dn.2xlarge, ml.c5.9xlarge, ml.g4dn.4xlarge, ml.c5.xlarge, ml.g4dn.16xlarge, 
+ml.c4.xlarge, ml.g4dn.8xlarge, ml.c5n.2xlarge, ml.c5n.4xlarge, ml.c5.18xlarge, ml.p3dn.24xlarge, ml.p3.2xlarge, 
+ml.m5.xlarge, ml.m4.10xlarge, ml.c5n.9xlarge, ml.m5.12xlarge, ml.m4.xlarge, ml.m5.24xlarge, ml.m4.2xlarge, 
+ml.p2.8xlarge, ml.m5.2xlarge, ml.p3.8xlarge, ml.m4.4xlarge] """
+
+
+"""
+S3_BUCKET = "wuerp-sagemaker"
+S3_PREDICT_PATH = "data/predict"
+# estimator.latest_training_job.name
+# 当训练和预测中断分离使，可手动对model_name赋值，拿到训练好的模型去预测
+# 特别重要，model_name是sagemaker,inference,models里面的模型名称，不是training jobs里面的模型名称，二者在时间戳上有时会有细微差别
+_transformer = sagemaker.transformer.Transformer(model_name='0042-top80test-week-2021-01-07-03-55-40-928',  # 特别重要，model_name是sagemaker,inference,models里面的模型名称，不是training jobs里面的模型名称，二者在时间戳上有时会有细微差别
+                                                instance_count=1,
+                                                instance_type="ml.c5.xlarge",
+                                                strategy="MultiRecord",
+                                                assemble_with="Line",
+                                                output_path=f"s3://{S3_BUCKET}/{S3_PREDICT_PATH}/out_0138/",
+                                                 tags=[{'Key': 'App', 'Value': 'Sagemaker-DeepAR'},
+                                                       {'Key': 'Depart', 'Value': 'RD7'},
+                                                       {'Key': 'Env', 'Value': 'Test'},
+                                                       {'Key': 'Manager', 'Value': 'Zhang Chi'},
+                                                       {'Key': 'Name', 'Value': 'DeepAR-Transform'}])
+
+_transformer.transform(f"s3://{S3_BUCKET}/{S3_PREDICT_PATH}/in_0138/",
+                          data_type="S3Prefix",
+                          split_type="Line",
+                          wait=False)
+
+"""
